648241baa5c50f5af6fd2a777ee27716115afdae
samuelbinogma <sbinogma41@gmail.com>
2025-10-05 18:51:07 +0000

    Use classList.add instead of className

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', () => {
         // Create remove button
         const removeButton = document.createElement('button');
         removeButton.textContent = 'Remove';
-        removeButton.className = 'remove-btn';
+        removeButton.classList.add('remove-btn');
 
         // Add click event to remove button to remove the list area
         removeButton.onclick = () => {
